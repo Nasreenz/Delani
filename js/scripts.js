f8ed2d877e684d63f270f38d9e46f04cfba86d77
Nasra Ibrahim <Nasrinanur@gmail.com>
2020-02-24 08:31:41 +0300

    Added javascript for the submit button

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     var email = $("input#numro1").val();
     var message = $("textarea#message").val();
     if ($("input#numro1").val() && $("input#numro1").val()){
-      alert (name + ", we have received your message. We appreciate you for reaching out to us.");
+      alert (name + ", we have received your message. we appreciate you for reaching out to us.");
     }
     else {
       alert("Please enter your name and email!");
